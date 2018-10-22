c9864db5108356f5ca45e361fb282e9651015203
prasetyo putra <prasetyo531@gmail.com>
2018-10-22 13:46:10 +0700

    add class java

--- src/test/java/com/qa/testcases/UpdateBeautyProfile.java
+++ src/test/java/com/qa/testcases/UpdateBeautyProfile.java
@@ -1,7 +1,7 @@
 package com.qa.testcases;
 
 public class UpdateBeautyProfile {
-    
+
 //    //get prop beauty profile
 //    String skintype = prop.getProperty("editskintype");
 //    String skintone = prop.getProperty("editskintone");
