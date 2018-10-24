c82bf11cd70a794692a5749ed37d1ad4f65c8b05
prasetyo putra <prasetyo531@gmail.com>
2018-10-24 18:30:13 +0700

    get data as array db con

--- src/main/java/com/qa/util/DatabaseConnection.java
+++ src/main/java/com/qa/util/DatabaseConnection.java
@@ -5,16 +5,58 @@ import java.util.ArrayList;
 
 public class DatabaseConnection {
 
-    public static String getDataAsArrayFirst(String query, String database) {
+    public static Object getDataUsername(String query, String database) {
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        Object username = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            switch (database) {
+                case "prod":
+                    con = (Connection) DriverManager.getConnection("jdbc:mysql://103.58.100.148/utstag2015", "qaeng", "7y@#ER7654#$%7ytf~!@#$%^87y");
+                    break;
+                case "staging":
+                    con = (Connection) DriverManager.getConnection("jdbc:mysql://54.169.68.90/staging_fdbr_salon", "serverteam", "DDKW31Kr31");
+                    break;
+                default:
+                    throw new Exception("No Database with that name");
+            }
+
+            stmt = (Statement) con.createStatement();
+            rs = stmt.executeQuery(query);
+            if (rs.next()) {
+                username = rs.getObject("username");
+            }
+
+        } catch (Exception e) {
+            return "Error:" + e.getMessage();
+        } finally {
+            try {
+                rs.close();
+                stmt.close();
+                con.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        return username;
+    }
+
+
+    public static String getDataBrandsArrayFirst(String query, String database) {
 
         Connection con = null;
         Statement stmt = null;
         ResultSet rs = null;
         Object output = null;
-        String prnt = null;
-        String prnt1 = null;
-        String prnt2 = null;
-        String prnt3 = null;
+        String brandName = null;
+        String brandName1 = null;
+        String brandName2 = null;
+        String brandName3 = null;
         ArrayList<prodItem> docters;
         docters = new ArrayList<prodItem>();
         try {
@@ -54,11 +96,67 @@ public class DatabaseConnection {
             }
         }
 
-        prnt = docters.get(0).getProdId();
-        prnt1 = docters.get(1).getProdId();
+        brandName = docters.get(0).getProdId();
+        brandName1 = docters.get(1).getProdId();
 //          prnt = output.toString();
 
-        return prnt;
+        return brandName;
+    }
+
+    public static String getDataProductArrayFirst(String query, String database) {
+
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        Object output = null;
+        String prodname = null;
+        String prodname1 = null;
+        String prodname2 = null;
+        ArrayList<prodName> product_Name;
+        product_Name = new ArrayList<prodName>();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            switch (database) {
+                case "prod":
+                    con = (Connection) DriverManager.getConnection("jdbc:mysql://103.58.100.148/utstag2015", "qaeng", "7y@#ER7654#$%7ytf~!@#$%^87y");
+                    break;
+                case "staging":
+                    con = (Connection) DriverManager.getConnection("jdbc:mysql://54.169.68.90/staging_fdbr_salon", "serverteam", "DDKW31Kr31");
+                    break;
+                default:
+                    throw new Exception("No Database with that name");
+            }
+
+            stmt = (Statement) con.createStatement();
+            rs = stmt.executeQuery(query);
+
+
+            while (rs.next()) {
+                output = rs.getObject("prod_item");
+                prodName pn = new prodName(output.toString());
+                product_Name.add(pn);
+            }
+
+        } catch (Exception e) {
+            return "Error:" + e.getMessage();
+        } finally {
+            try {
+                rs.close();
+                stmt.close();
+                con.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        prodname = product_Name.get(0).getProdName();
+        prodname1 = product_Name.get(1).getProdName();
+        prodname2 = product_Name.get(2).getProdName();
+//          prnt = output.toString();
+
+        return prodname;
     }
 
 }
---
+++ src/main/java/com/qa/util/prodName.java
@@ -0,0 +1,16 @@
+package com.qa.util;
+
+public class prodName {
+
+    String prodName;
+
+    public prodName(String string) {
+        // TODO Auto-generated constructor stub
+        this.prodName = prodName;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+}
